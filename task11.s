df41258daaf0c49f5abd3ac92cc9273d930a3349
Vladik <git.work2808@gmail.com>
2025-09-30 22:43:41 +0900

    namespace

---
+++ task11.s
@@ -0,0 +1,117 @@
+	.file	"task11a.cpp"
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "ru_RU.UTF-8\0"
+	.align 8
+.LC1:
+	.ascii "\320\222\320\262\320\265\320\264\320\270\321\202\320\265 x, y, z \321\207\320\265\321\200\320\265\320\267 \320\277\321\200\320\276\320\261\320\265\320\273: \0"
+.LC2:
+	.ascii "\320\240\320\265\320\267\321\203\320\273\321\214\321\202\320\260\321\202:\12\0"
+.LC3:
+	.ascii "a = \0"
+.LC4:
+	.ascii "\12\0"
+.LC5:
+	.ascii "b = \0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB2189:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$80, %rsp
+	.seh_stackalloc	80
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdx
+	movl	$0, %ecx
+	call	setlocale
+	leaq	.LC1(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	-24(%rbp), %rdx
+	movq	.refptr._ZSt3cin(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSirsERd
+	movq	%rax, %rcx
+	leaq	-32(%rbp), %rax
+	movq	%rax, %rdx
+	call	_ZNSirsERd
+	movq	%rax, %rcx
+	leaq	-40(%rbp), %rax
+	movq	%rax, %rdx
+	call	_ZNSirsERd
+	movsd	-32(%rbp), %xmm0
+	movq	-24(%rbp), %rax
+	movapd	%xmm0, %xmm1
+	movq	%rax, %xmm0
+	call	_ZN4calc6calc_aEdd
+	movq	%xmm0, %rax
+	movq	%rax, -8(%rbp)
+	movsd	-40(%rbp), %xmm0
+	movq	-24(%rbp), %rax
+	movapd	%xmm0, %xmm1
+	movq	%rax, %xmm0
+	call	_ZN4calc6calc_bEdd
+	movq	%xmm0, %rax
+	movq	%rax, -16(%rbp)
+	leaq	.LC2(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	.LC3(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movsd	-8(%rbp), %xmm0
+	movapd	%xmm0, %xmm1
+	movq	%rax, %rcx
+	call	_ZNSolsEd
+	movq	%rax, %rcx
+	leaq	.LC4(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	.LC5(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movsd	-16(%rbp), %xmm0
+	movapd	%xmm0, %xmm1
+	movq	%rax, %rcx
+	call	_ZNSolsEd
+	movq	%rax, %rcx
+	leaq	.LC4(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	$0, %eax
+	addq	$80, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev5, Built by MSYS2 project) 15.1.0"
+	.def	setlocale;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	_ZNSirsERd;	.scl	2;	.type	32;	.endef
+	.def	_ZN4calc6calc_aEdd;	.scl	2;	.type	32;	.endef
+	.def	_ZN4calc6calc_bEdd;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEd;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt3cin, "dr"
+	.p2align	3, 0
+	.globl	.refptr._ZSt3cin
+	.linkonce	discard
+.refptr._ZSt3cin:
+	.quad	_ZSt3cin
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.p2align	3, 0
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
